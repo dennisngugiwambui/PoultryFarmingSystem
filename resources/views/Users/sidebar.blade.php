5577f98d234ea3471ea94d25dcd2ea573d6deac0
Denno <dellyit001@gmail.com>
2024-03-20 13:50:53 -0700

    users pages

---
+++ resources/views/Users/sidebar.blade.php
@@ -0,0 +1,19 @@
+***********************************-->
+<div class="quixnav">
+    <div class="quixnav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="nav-label first">Main Menu</li>
+            <li><a class="" href="/dashboard" aria-expanded="false"><i
+                        class="icon icon-single-04"></i>Dashboard</a>
+            </li>
+            <li><a class="" href="/profile" aria-expanded="false"><i
+                        class="icon icon-single-04"></i>Profile</a>
+            </li>
+        </ul>
+    </div>
+
+
+</div>
+<!--**********************************
+    Sidebar end
+***********************************-->
