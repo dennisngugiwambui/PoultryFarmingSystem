7282c0dd61450ec17ea0c6f0b037efa15011d77c
Denno <dellyit001@gmail.com>
2024-03-21 10:56:21 -0700

    users dashboard

--- resources/views/Users/eggs.blade.php
+++ resources/views/Users/eggs.blade.php
@@ -134,7 +134,7 @@
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="submit" class="btn btn-primary">Save Changes</button>
+                                    <button type="submit" class="btn btn-primary">Save</button>
                                 </div>
                             </form>
                         </div>
